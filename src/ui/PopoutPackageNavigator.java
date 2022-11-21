9053392f74c4faa39e9b2341846144b1803736db
Ada <39278228+Reithger@users.noreply.github.com>
2022-11-21 11:59:57 -0500

    Fixed scrolling issue in popout window for ignored packages

--- src/ui/PopoutPackageNavigator.java
+++ src/ui/PopoutPackageNavigator.java
@@ -55,17 +55,18 @@ public class PopoutPackageNavigator extends PopoutWindow{
 	private void drawPage() {
 		int maxY = drawNode(root, p.getWidth() / 20, p.getHeight() / 20);
 		p.handleRectangle("spacer", "no_move", 15, p.getWidth() / 2, maxY + p.getHeight() / 15, 5, 5, Color.white, Color.white);
-		p.handleTextButton("submit", "move", 5, p.getWidth() - p.getWidth() / 6, p.getHeight() / 20, p.getWidth() / 5, p.getHeight() / 16, DEFAULT_FONT, "Submit", CODE_SUBMIT, Color.green, Color.black);
 		p.addScrollbar("scrollbar", 20, "no_move", p.getWidth() - p.getWidth() / 20, 0, p.getWidth() / 20, p.getHeight(), 0, p.getHeight(), "move", true);
+		p.handleTextButton("submit", "no_move", 5, p.getWidth() - p.getWidth() / 6, p.getHeight() / 20, p.getWidth() / 5, p.getHeight() / 16, DEFAULT_FONT, "Submit", CODE_SUBMIT, Color.green, Color.black);
 	}
 	
 	private int drawNode(Node n, int x, int y) {
 		int iconSize = p.getHeight() / 20;
-		p.handleImageButton("node_butt_" + n.getName() + "_" + x + "_" + y, "no_move", 10, x, y, iconSize, iconSize, n.getStatus() ? ICON_PATH_OPEN : ICON_PATH_CLOSED, n.getCode());
-		p.addText("node_text_" + n.getName() + "_" + x + "_" + y, 15, "no_move", x + iconSize, y, p.getWidth() / 3, p.getHeight() / 15, n.getName() + "/", DEFAULT_FONT, false, true, false);
+		p.handleImageButton("node_butt_" + n.getName() + "_" + x + "_" + y, "move", 10, x, y, iconSize, iconSize, n.getStatus() ? ICON_PATH_OPEN : ICON_PATH_CLOSED, n.getCode());
+		int textWid = this.getHandlePanel().getTextWidth(n.getName() + "/", DEFAULT_FONT);
+		p.addText("node_text_" + n.getName() + "_" + x + "_" + y, 15, "move", x + iconSize / 2 + textWid / 2, y, textWid + 2 * iconSize, p.getHeight() / 15, n.getName() + "/", DEFAULT_FONT, true, true, true);
 		y += p.getHeight() / 15;
 		if(n.getStatus() && n.getChildren().size() > 0)
-			p.handleImage("lbend_" + n.getName() + "_" + x + "_" + y, "no_move", 5, x, y, ICON_BEND, 1);
+			p.handleImage("lbend_" + n.getName() + "_" + x + "_" + y, "move", 5, x, y, ICON_BEND, 1);
 		x += p.getWidth() / 10;
 		if(n.getStatus()) {
 			for(Node n2 : n.getChildren()) {
